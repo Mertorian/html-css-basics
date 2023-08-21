9e5bb437e0e4584912f82a94c7444142aa11d61b
Mert Tercan <AlfredTheDuck@gmx.de>
2023-08-21 16:58:36 +0200

    new repo who dis

---
+++ html-basics/CGPT-Test/script.js
@@ -0,0 +1,6 @@
+// Adding a simple click event to the header for interactivity
+const header = document.querySelector("header");
+
+header.addEventListener("click", () => {
+  alert("Hello! This is Mert Tercan's profile.");
+});
