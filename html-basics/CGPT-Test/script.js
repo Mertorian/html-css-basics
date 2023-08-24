ab2872e873410c8dd17e91d93d7ba9f9026abef9
Mert Tercan <AlfredTheDuck@gmx.de>
2023-08-24 14:16:26 +0200

    add new stuff

--- html-basics/CGPT-Test/script.js
+++ html-basics/CGPT-Test/script.js
@@ -4,3 +4,17 @@ const header = document.querySelector("header");
 header.addEventListener("click", () => {
   alert("Hello! This is Mert Tercan's profile.");
 });
+
+// ... (existing code as before) ...
+
+// Switch between pages when the buttons are clicked
+const profileButton = document.getElementById("profileButton");
+const contactButton = document.getElementById("contactButton");
+
+profileButton.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
+
+contactButton.addEventListener("click", () => {
+  window.location.href = "contact.html";
+});
